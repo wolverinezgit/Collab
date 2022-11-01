afca5a93a732460aa133f37e36f2b3539ce57130
wolverinezgit <harshsrivastava9795@gmail.com>
2022-11-01 19:38:24 +0530

    crated react routes for homepage and editorpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Homepage from "./Pages/Homepage"
+import Editorpage from "./Pages/Editorpage"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+      
+      <Route path="/" element={<Homepage/>}></Route>
+      <Route path="/editor/:id" element={<Editorpage/>}> </Route>
+     
+      </Routes>
+      
+      
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/Editorpage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Editorpage() {
+  return (
+    <div>Editorpage</div>
+  )
+}
+
+export default Editorpage
